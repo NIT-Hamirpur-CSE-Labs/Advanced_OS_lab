8bbd43ea59dad9768e220cf169c5a6f84ddd9bef
Rishabh04-02 <rishabh0402@gmail.com>
2017-11-20 22:13:00 +0530

    lab 2 programs in python added

---
+++ Lab_2_server.py
@@ -0,0 +1,27 @@
+import socket
+
+
+def Main():
+	host = '127.0.0.1'
+	port = 5000
+
+	s = socket.socket()
+	s.bind((host, port))
+
+	s.listen(1)
+	c,addr = s.accept()
+
+	print "connection from : " + str(addr)
+
+	while True:
+		data = c.recv(1024)
+		if not data:
+			break
+		print "client :" + str(data)
+
+		data = raw_input('-->')
+		print "sending " + str(data)
+		c.send(data)
+	c.close()
+if __name__ == '__main__':
+	Main();
